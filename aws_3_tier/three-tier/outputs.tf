f8dc238d9781d70f5de35f52dd378a5b5149adeb
risingskyscrapper <risingskyscrapper@gmail.com>
2021-06-19 12:46:32 +0530

    Started working on modules

---
+++ aws_3_tier/three-tier/outputs.tf
@@ -0,0 +1,17 @@
+output "nginxip" {
+  value = "${aws_instance.nginx.public_ip}"
+}
+
+output "tomcat1ip" {
+  value = "${aws_instance.tomcat1.public_ip}"
+}
+
+
+output "tomcat2ip" {
+  value = "${aws_instance.tomcat2.public_ip}"
+}
+
+output "dbendpoint" {
+    value = "${aws_db_instance.db1.endpoint}"
+  
+}
